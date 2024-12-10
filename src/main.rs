1128d23317b1e85048d41d9ba5e9dd6b48d4c2d4
yomi <83850050+yomi2@users.noreply.github.com>
2024-12-09 20:04:54 -0400

    -1 Validated

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{get, web::ServiceConfig, HttpResponse };
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
@@ -6,10 +6,22 @@ async fn hello_world() -> &'static str {
     "Hello, bird!"
 }
 
+#[get("/-1/seek")]
+
+
+async fn task2() -> HttpResponse{
+    HttpResponse::Found()
+        .insert_header(("Location","https://www.youtube.com/watch?v=9Gc4QTqslN4"))
+        .finish()
+    
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
+        cfg.service(task2);
+        
     };
 
     Ok(config.into())
